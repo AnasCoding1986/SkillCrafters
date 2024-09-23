1ecae5373ad3fc8d4da705d53c66511c384ee4de
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-09-23 07:35:26 +0600

    job details done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,34 +1,41 @@
 import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home";
-import Login  from "../pages/Authentication/Login";
-import Register  from "../pages/Authentication/Register";
+import Login from "../pages/Authentication/Login";
+import Register from "../pages/Authentication/Register";
 import ErrorPage from "../pages/ErrorPage";
+import JobDetails from "../components/JobDetails";
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      errorElement: <ErrorPage></ErrorPage>,
-      children: [
-        {
-          index: true,
-          element: <Home></Home>
-        },
-        {
-          path: "/login",
-          element: <Login></Login>
-        },
-        {
-          path: "/registration",
-          element: <Register></Register>
-        },
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/registration",
+        element: <Register></Register>
+      },
+      {
+        path: "/job/:id",
+        element: <JobDetails></JobDetails>,
+        loader: ({ params }) =>
+          fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`)
+      },
 
-      ],
-    },
-  ]);
+    ],
+  },
+]);
 
-  export default router
+export default router
